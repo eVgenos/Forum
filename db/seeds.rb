9fe3719a57c67fc427d7b43ffe18754a9c6658b8
eVgenos <evgenos2709@gmail.com>
2017-02-11 00:37:09 +0200

    Add Bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.delete_all
+User.delete_all
+user = User.create!(name: 'Evgeniy', email: 'evgenos2709@gmail.com', password: '1111')
+
+categories = %w(HTML Ruby JavaScript PHP)
+
+categories.each do |c|
+    user.categories.create!(title: c)
+end
