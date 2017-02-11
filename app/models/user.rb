9aa2a4477c45b59dd1ae50811f12dd5d902fbf89
eVgenos <evgenos2709@gmail.com>
2017-02-12 01:55:38 +0200

    Add header and footer. Add validates for user. Add few bootstrap classes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :name, :email, :password, presence: true
   validates :password, length: 4..16
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, uniqueness: true
 
   has_many :categories
   has_many :messages
