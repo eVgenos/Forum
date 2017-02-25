16c3fdc3e873e857f7c0895b47b0a2f35c56b820
eVgenos <evgenos2709@gmail.com>
2017-02-25 18:25:08 +0200

    Change per_page for users

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,7 +3,7 @@ class AdminController < ApplicationController
   before_action :authorize
 
   def index
-    @users = User.all.paginate(page: params[:page], per_page: 2)
+    @users = User.all.paginate(page: params[:page], per_page: 10)
   end
 
   def update
