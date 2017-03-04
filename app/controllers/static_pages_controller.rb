e6f33e18f50ebb10f8bf17b75f81e6154b01bb2b
eVgenos <evgenos2709@gmail.com>
2017-03-04 15:34:36 +0200

    Add style for landing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    @last_categories = Category.published.limit(3)
+    @categoires = Category.published
+    @users = User.notdeleted
+    @messages = Message.all
   end
 end
